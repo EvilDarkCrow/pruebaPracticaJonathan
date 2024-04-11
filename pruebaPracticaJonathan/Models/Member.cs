951ee475097ade3d89ff31fc069ce86bef2bb848
Jonathan <jonathanmorales1735@gmail.com>
2024-04-11 13:17:32 +0200

    Agregar archivos de proyecto.

---
+++ pruebaPracticaJonathan/Models/Member.cs
@@ -0,0 +1,10 @@
+﻿namespace pruebaPracticaJonathan.Models
+{
+    public class Member
+    {
+        public long id {  get; set; }
+        public string name { get; set; }
+
+        
+    }
+}
---
+++ pruebaPracticaJonathan/Models/Product.cs
@@ -0,0 +1,8 @@
+﻿namespace pruebaPracticaJonathan.Models
+{
+    public class Product
+    {
+        public long Id { get; set; }
+        public string name { get; set; }
+    }
+}
