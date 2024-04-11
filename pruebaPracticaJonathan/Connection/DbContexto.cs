6e41effcff9c4bb8f44efa26121eacc6a719d587
Jonathan <jonathanmorales1735@gmail.com>
2024-04-11 14:35:30 +0200

    Migracion hecha

---
+++ pruebaPracticaJonathan/Connection/DbContexto.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.EntityFrameworkCore;
+using pruebaPracticaJonathan.Models;
+
+namespace pruebaPracticaJonathan.Connection
+{
+    public class DbContexto:DbContext
+    {
+        
+
+        public DbContexto(DbContextOptions<DbContexto> options) : base(options)
+        {
+
+        }
+
+        public DbSet<Member> members { get; set; }
+        public DbSet<Order> orders { get; set; }
+        public DbSet<Product> products { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Order>()
+                .HasOne(o => o.member)
+                .WithMany()
+                .HasForeignKey(o => o.MemberId)
+                .OnDelete(DeleteBehavior.Restrict);
+
+            modelBuilder.Entity<Order>()
+                .HasOne(o => o.product)
+                .WithMany()
+                .HasForeignKey(o => o.ProductId)
+                .OnDelete(DeleteBehavior.Restrict);
+        }
+
+    }
+}
--- pruebaPracticaJonathan/Program.cs
+++ pruebaPracticaJonathan/Program.cs
@@ -1,3 +1,6 @@
+using Microsoft.EntityFrameworkCore;
+using pruebaPracticaJonathan.Connection;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +9,7 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.AddDbContext<DbContexto>(options => options.UseSqlServer(builder.Configuration.GetConnectionString("conexionmaestra")));
 
 var app = builder.Build();
 
